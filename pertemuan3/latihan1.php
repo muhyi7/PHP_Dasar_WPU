8418830e28dc52e66dd67f1d1bead744ad6afdd5
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-02-08 14:16:10 +0700

    latihan 1 end

--- pertemuan3/latihan1.php
+++ pertemuan3/latihan1.php
@@ -8,3 +8,15 @@
 // for ($i = 0; $i < 5; $i++) {
 //     echo "Hello World! <br> ";
 // }
+
+// $i = 0;
+// while ($i < 5) {
+//     echo "Hello World! <br> ";
+//     $i++;
+// }
+
+$i = 10;
+do{
+    echo "Hello World! <br> ";
+    $i++;
+} while($i < 5);
