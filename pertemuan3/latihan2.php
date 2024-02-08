918d5f141269abdaa5202dbe085bdd65916fd132
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-02-08 14:28:21 +0700

    latihan 2

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,15 @@
+<?php
+// Pengkondisian /Percabangan 
+// if else 
+// if else if else
+
+$x = 2;
+if( $x < 20){
+    echo "benar";
+} else if($x == 20){
+    echo "bingo!";
+} else{
+    echo "salah";
+}
+
+?>
